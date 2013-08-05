a28017af90dde08d7a3c228217435908a1e72690
System Administrator <root@melissas-Mac.local>
2013-08-05 16:52:01 -0700

    clean things up

--- git/manifests/init.pp
+++ git/manifests/init.pp
@@ -4,24 +4,20 @@ class git {
   include git::params
 
   if $operatingsystem == 'Darwin' {
-     $providers = $git::params::providers
-     $sources = $git::params::sources
-     file { '/usr/local/bin':
-       ensure => directory,
-       require => Package[$git::params::packages],
-     }
-     file { '/usr/local/bin/git':
-       ensure => link,
-       target => '/usr/local/git/bin/git',
-       require => File['/usr/local/bin'],
-     }
-   }
+    $sources = $git::params::sources
+    file { '/usr/local/bin':
+      ensure => directory,
+      require => Package[$git::params::packages],
+    }
+    file { '/usr/local/bin/git':
+      ensure => link,
+      target => '/usr/local/git/bin/git',
+      require => File['/usr/local/bin'],
+    }
+  }
 
   package { $git::params::packages:
     ensure => installed,
-    if $operatingsystem == 'Darwin' {
-      provider => $providers,
-      source => $sources,
-    }
+    source => $sources,
   }
 }
--- git/manifests/params.pp
+++ git/manifests/params.pp
@@ -14,7 +14,6 @@ class git::params {
     'FreeBSD': { $packages = 'devel/git' }
     'Darwin': {
       $packages = "git"
-      $providers = 'pkgdmg'
       $sources = "https://git-osx-installer.googlecode.com/files/git-1.8.3.2-intel-universal-snow-leopard.dmg"
     }
     default:   { fail("No git package known for operating system ${operatingsystem}") }
