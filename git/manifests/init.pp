02f789629e9e1e85217ac9a5c369d8120ae4b487
System Administrator <root@melissas-Mac.local>
2013-08-05 17:10:41 -0700

    playing with this to see if I can mess it up/clean it up

--- git/manifests/init.pp
+++ git/manifests/init.pp
@@ -3,21 +3,8 @@ class git {
 
   include git::params
 
-  if $operatingsystem == 'Darwin' {
-    $sources = $git::params::sources
-    file { '/usr/local/bin':
-      ensure => directory,
-      require => Package[$git::params::packages],
-    }
-    file { '/usr/local/bin/git':
-      ensure => link,
-      target => '/usr/local/git/bin/git',
-      require => File['/usr/local/bin'],
-    }
-  }
-
   package { $git::params::packages:
     ensure => installed,
-    source => $sources,
+    source => $git::params::sources,
   }
 }
--- git/manifests/params.pp
+++ git/manifests/params.pp
@@ -15,6 +15,15 @@ class git::params {
     'Darwin': {
       $packages = "git"
       $sources = "https://git-osx-installer.googlecode.com/files/git-1.8.3.2-intel-universal-snow-leopard.dmg"
+      file { '/usr/local/bin':
+        ensure => directory,
+        require => Package[$git::params::packages],
+      }
+      file { '/usr/local/bin/git':
+        ensure => link,
+        target => '/usr/local/git/bin/git',
+        require => File['/usr/local/bin'],
+      }
     }
     default:   { fail("No git package known for operating system ${operatingsystem}") }
   }
